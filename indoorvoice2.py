a6ed8f3d043262f11ce7bf7dffd7d3159da62996
Creative2023 <104870390+Creative2023@users.noreply.github.com>
2022-06-16 15:13:26 -0400

    Add files via upload

---
+++ indoorvoice2.py
@@ -0,0 +1,16 @@
+#Indoor Voice. Prompt the user for input and then outputs that same input in lowercase.
+
+voice = input("Please greet me with Hello. ")
+
+#Remove white space from voice
+
+voice= voice.strip()
+
+#Make user response lowercase
+
+voice = voice.lower()
+
+#Say hello and thank you to user.
+print(f"Hello,notice your hello is in lower case." voice.)
+
+
