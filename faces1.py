e9c500f480373b01cd879d028f9f9f2dad37d5ff
Creative2023 <104870390+Creative2023@users.noreply.github.com>
2022-06-16 15:18:23 -0400

    Add files via upload

---
+++ faces1.py
@@ -0,0 +1,19 @@
+#Convert a :) into a emoji and a :( into and emoji
+#Define happy
+
+happy="Hello, :)"
+print(happy) 
+
+happi=happy.replace(":)","🙂" )
+print(happi)
+
+#Say goodbye
+
+bye = "Goodbye,:)"
+print(bye)
+
+byi=bye.replace(":)", "☹")
+print(byi)
+
+
+
