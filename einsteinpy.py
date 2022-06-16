4dd983c9dceb56bd4d8e60b85bc659068d2ab3ed
Creative2023 <104870390+Creative2023@users.noreply.github.com>
2022-06-16 17:27:54 -0400

    Add files via upload

---
+++ einsteinpy.py
@@ -0,0 +1,6 @@
+#Ask user for a unit(s) of mass lbs.
+mass1=input("Please enter the number of kilograms for the amount of mass of an object." ) 
+mass1=float(mass1)
+mass2 = float(mass1 * 4.5244E+41)
+e=float(mass1 * 3000000000** 2)
+print(e)
